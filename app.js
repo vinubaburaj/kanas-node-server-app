dcc5c8432d509fabe2743ee12cc8b75af62e02a2
Vinu Baburaj <vinubaburaj@gmail.com>
2023-11-09 00:35:49 -0500

    Labs Data from server

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import Hello from "./hello.js";
 import Lab5 from "./Lab5.js";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 Lab5(app);
 Hello(app);
