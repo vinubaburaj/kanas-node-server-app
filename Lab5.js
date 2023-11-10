9e9bdf9a5f636998a90e7cfb13356e6ca75a68ff
Vinu Baburaj <vinubaburaj@gmail.com>
2023-11-10 01:12:34 -0500

    Implemented Labs

--- Lab5.js
+++ Lab5.js
@@ -17,6 +17,46 @@ const todos = [
 
 const Lab5 = (app) => {
 
+    app.put("/a5/todos/:id", (req, res) => {
+        const { id } = req.params;
+        const todo = todos.find((t) => t.id === parseInt(id));
+        if (!todo) {
+            res.res
+                .status(404)
+                .json({ message:
+                        `Unable to update Todo with ID ${id}` });
+            return;
+        }
+        todo.title = req.body.title;
+        todo.description = req.body.description;
+        todo.due = req.body.due;
+        todo.completed = req.body.completed;
+        res.sendStatus(200);
+    });
+
+    app.delete("/a5/todos/:id", (req, res) => {
+        const { id } = req.params;
+        const todo = todos.find((t) => t.id === parseInt(id));
+        if(!todo){
+            res.res
+                .status(404)
+                .json({ message:
+                        `Unable to delete Todo with ID ${id}` });
+            return;
+        }
+        todos.splice(todos.indexOf(todo), 1);
+        res.sendStatus(200);
+    });
+
+    app.post("/a5/todos", (req, res) => {
+        const newTodo = {
+            ...req.body,
+            id: new Date().getTime(),
+        };
+        todos.push(newTodo);
+        res.json(newTodo);
+    });
+
     app.get("/a5/todos", (req, res) => {
         const { completed } = req.query;
         if(completed !== undefined){
