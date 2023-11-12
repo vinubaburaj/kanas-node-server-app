110f1b6972bc3de20b912129ef7eea9f0a0da9be
Vinu Baburaj <vinubaburaj@gmail.com>
2023-11-12 12:13:35 -0500

    Implemented Modules

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ import Hello from "./hello.js";
 import Lab5 from "./Lab5.js";
 import cors from "cors";
 import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./modules/routes.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 CourseRoutes(app);
+ModuleRoutes(app);
 Lab5(app);
 Hello(app);
 app.listen(4000);
