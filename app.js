031967053cc483fca5ecd526e07754871773c1e8
Vinu Baburaj <vinubaburaj@gmail.com>
2023-11-28 19:47:53 -0500

    Added mongo atlas link

--- app.js
+++ app.js
@@ -9,8 +9,12 @@ import 'dotenv/config';
 import mongoose from "mongoose";
 import UserRoutes from "./users/routes.js";
 import session from "express-session";
+import "dotenv/config";
 
-mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
+
+
+mongoose.connect(CONNECTION_STRING);
 
 const app = express();
 
@@ -24,9 +28,16 @@ const sessionOptions = {
     resave: false,
     saveUninitialized: false,
 };
-app.use(
-    session(sessionOptions)
-);
+
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+    };
+}
+app.use(session(sessionOptions));
+
 
 app.use(express.json());
 CourseRoutes(app);
